3ec551a7de3060ece3c5542dcffef603fb0930e9
Langbien <pierre.langbien@gmail.com>
2022-11-09 14:03:53 +0100

    14h

---
+++ script.js
@@ -0,0 +1,10 @@
+//let tripList = fetch(`http://localhost:3000/trips`)
+// .then(response => response.json())
+// .then ( data => {
+
+    document.querySelector('#results').innerHTML = 
+    ``
+    
+   
+// })
+ 
